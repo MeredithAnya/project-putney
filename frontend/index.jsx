52a736fcc53704e3a76f145077df678786739778
Meredith Heller <meredith.a.heller@gmail.com>
2016-07-28 13:17:37 -0700

    test entry page

---
+++ frontend/index.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function(){
+  ReactDom.render(<div>hello</div>, document.getElementById('content'))
+});
